10d37f161c8627a75b186223a4249145612683d1
nabhanrashid <nabhandb06@gmail.com>
2025-09-22 16:05:31 -0400

    Finished JSONTranslator

--- src/main/java/translation/JSONTranslator.java
+++ src/main/java/translation/JSONTranslator.java
@@ -51,13 +51,16 @@ public class JSONTranslator implements Translator {
 
                 List<String> languages = new ArrayList<>();
 
-                // TODO Task C: record this countryCode in the correct instance variable
+                countryCodes.add(countryCode);
 
                 // iterate through the other keys to get the information that we need
                 for (String key : countryData.keySet()) {
                     if (!key.equals("id") && !key.equals("alpha2") && !key.equals("alpha3")) {
                         String languageCode = key;
-                        // TODO Task C: record this translation in the appropriate instance variable
+                        if (!languageCodes.contains(languageCode)) {
+                            languageCodes.add(languageCode);
+                        }
+                        translations.put(countryCode + "-" + languageCode, countryData.getString(key));
 
                         if (!languages.contains(languageCode)) {
                             languages.add(languageCode);
@@ -86,5 +89,9 @@ public class JSONTranslator implements Translator {
     public String translate(String countryCode, String languageCode) {
         // TODO Task C: complete this method using your instance variables as needed
         return "JSONTranslator's translate method is not implemented!";
+    }
+
+    public static void main(String[] args) {
+        JSONTranslator lator = new JSONTranslator();
     }
 }
